349c44ab1f8efb7a7903f78cae5601e41cd62b06
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-06 00:09:50 +0200

    valid parentheses solution

---
+++ Stack/valid-parentheses.js
@@ -0,0 +1,14 @@
+ function isValid(s) {
+    const stack = [];
+    const map = {')':'(', ']':'[', '}':'{'};
+
+    for (let char of s) {
+        if (map[char]) {
+            if (stack.pop() !== map[char]) return false;
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
