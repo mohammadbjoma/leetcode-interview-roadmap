4bc21f28c9720f4dd6944335382fa9790106654f
mohammad joma <150864877+mohammadbjoma@users.noreply.github.com>
2026-01-16 21:20:45 +0200

    climbing-stairs solution

---
+++ dynamic-programming/climbing-stairs.js
@@ -0,0 +1,14 @@
+var climbStairs = function(n) {
+    if (n <= 2) return n;
+
+    let prev2 = 1; 
+    let prev1 = 2; 
+
+    for (let i = 3; i <= n; i++) {
+        let current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+
+    return prev1;
+};
